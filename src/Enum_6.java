75d9238116f8ed58aa815f5c477bcfaaf48a5c40
gcronax <guillecampher15@gmail.com>
2025-01-13 20:47:14 +0100

    WERWEREWREQWEQW

--- src/Enum_6.java
+++ src/Enum_6.java
@@ -1,19 +1,40 @@
 public class Enum_6 {
     enum Pedidos{
         RECIBIDO,PREPARACION,LISTO,ENTREGADO;
+
+
+
+
+
     }
 
     public static Pedidos cambiarpedido(Pedidos p){
 
-        if (p== Pedidos.RECIBIDO){
-            p= Pedidos.PREPARACION;
-            System.out.println("se cambia a preparacion");
+        switch (p){
+            case RECIBIDO -> {
+                p= Pedidos.PREPARACION;
+                System.out.println("se cambia a PREPARACION");
+            }
+            case PREPARACION -> {
+                p= Pedidos.LISTO;
+                System.out.println("se cambia a LISTO");
+            }
+            case LISTO -> {
+                p= Pedidos.ENTREGADO;
+                System.out.println("se cambia a ENTREGADO");
+            }
+
         }
 
         return p;
     }
 
     public static void main(String[] args) {
-        cambiarpedido(Pedidos.RECIBIDO);
+        Pedidos p= Pedidos.RECIBIDO;
+        p=cambiarpedido(p);
+        p=cambiarpedido(p);
+        System.out.println(p);
+
+
     }
 }

4a62753fce950840bf9d3176d7a052a94ca01104
gcronax <guillecampher15@gmail.com>
2025-01-13 19:27:26 +0100

    WERWEREWREQWEQW

--- src/colas_1.java
+++ src/colas_1.java
@@ -1,2 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class colas_1 {
+    public static void main(String[] args) {
+        Queue<String> filabanco = new LinkedList<>();
+        for (int i = 1; i <= 10 ; i++) {
+
+            filabanco.add("cliente "+i);
+        }
+        System.out.println("se atendio al cliente "+filabanco.poll());
+        System.out.println("se atendio al cliente "+filabanco.poll());
+        System.out.println("se atendio al cliente "+filabanco.poll());
+        System.out.println("se atendio al cliente "+filabanco.poll());
+        System.out.println("estado de la cola actual "+filabanco);
+
+    }
 }
